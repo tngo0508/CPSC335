36adfd7997cdc53978c9391f04aeb7394495ede5
Thomas Ngo <tngo0508@gmail.com>
2017-04-15 13:30:23 -0700

    fix author's names

--- project3/EOA.cpp
+++ project3/EOA.cpp
@@ -1,5 +1,6 @@
 // Assignment 3: Rectilinear traveling salesperson problem: improved nearest neighbor algorithm
-// XX YY ( YOU NEED TO COMPLETE YOUR NAME )
+// THOMAS NGO
+// VAN DO
 // A special case of the classical traveling salesman problem (TSP) where the input is a Euclidean graph
 // INPUT: a positive integer n and a list P of n distinct points representing vertices of a Euclidean graph
 // OUTPUT: a list of n points from P representing a Hamiltonian cycle of relatively minimum total weight
